88413fd290be78e0998ae82c4482f51f0db32bc2
mcjisok <353750533@qq.com>
2018-06-09 17:15:57 +0800

    修改标签控制器

--- app/controller/tag.js
+++ app/controller/tag.js
@@ -1,26 +1,58 @@
 const Tag = require('../models/tag')
+const SubTag = require('../models/subtag')
+
 
 module.exports = {
     saveTag:async(ctx,next)=>{
-        let name = ctx.request.body.name
-        let data = {
-            tagName:name
-        }
-        console.log(data)
-        let newTag = new Tag(data)
-        console.log(newTag)
-        let save = await newTag.save()
-        console.log(save)
-        if(save){
-            ctx.response.body = {code:200,data:save,message:"保存成功"}
+
+        let body = ctx.request.body
+        if(body.firstTagID){
+            let data = {
+                pTag:body.firstTagID,
+                tagName:body.subTagName
+            }
+            let newdata = new SubTag(data)
+            let save = await newdata.save()
+
+            let firstTag = await Tag.findOne({_id:body.firstTagID}).exec()
+            console.log(firstTag)
+            firstTag.subTagList.unshift(save._id)
+            let firstTagUpdate = await firstTag.save()
+            // console.log(res)
+
+            let success = Promise.all([save,firstTagUpdate])
+            .then(res=>{
+                ctx.response.body = {code:200,data:save,message:"保存成功"}                
+            })
+            .catch(e=>{
+                ctx.response.body = {code:400,data:null,message:e}                
+            })
+            
         }
         else{
-            ctx.response.body = {code:400,data:null,message:"保存失败"}
+            let name = ctx.request.body.name
+            let data = {
+                tagName:name
+            }
+            console.log(ctx.request.body)
+            let newTag = new Tag(data)
+            console.log(newTag)
+            let save = await newTag.save()
+            console.log(save)
+            if(save){
+                ctx.response.body = {code:200,data:save,message:"保存成功"}
+            }
+            else{
+                ctx.response.body = {code:400,data:null,message:"保存失败"}
+            }
         }
+        
     },
 
     getTagList:async(ctx,next)=>{
-        let tagList = await Tag.fetch()
+        let tagList = await Tag.find()
+                        .populate({path:'subTagList'})
+                        .exec()
         // console.log('rere')
         console.log(tagList)
         if(tagList){
--- app/models/subtag.js
+++ app/models/subtag.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const SubTagSchema = require('../schemas/tag')
+const SubTagSchema = require('../schemas/subtag')
 const SubTag = mongoose.model('SubTag', SubTagSchema)
 
 module.exports = SubTag
